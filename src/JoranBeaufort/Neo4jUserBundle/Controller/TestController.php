39a03c5eff7c85dd56b3139a79b7af66ca04ffca
mfbaer@gmail.com <mbaer@geonet.ch>
2016-07-27 12:56:09 -0500

    Changed warning

--- src/JoranBeaufort/Neo4jUserBundle/Controller/TestController.php
+++ src/JoranBeaufort/Neo4jUserBundle/Controller/TestController.php
@@ -199,6 +199,17 @@ class TestController extends Controller
 
         $em = $this->get('neo4j.graph_manager')->getClient();
         
+        // DROP ALL DATA IN GRAPH !!!CAREFULL!!!:
+        $em->getDatabaseDriver()->run("match (n)-[r]-() delete r,n");
+        $em->getDatabaseDriver()->run("match (n) delete n");
+        echo '<p style="color:green">DB CLEARED</p><br>';        
+        
+        // QUERY TO CREATE TEST DATA IN DB
+        $em->getDatabaseDriver()->run("create (a:Resources{resourceType:'wood',name_DE:'Holz',icon:'fa-tree',iconColour:'#fff',colour:'#00C851'}) create (b:Resources{resourceType:'stone',name_DE:'Stein',icon:'fa-cubes',iconColour:'#fff',colour:'#a1887f'}) create (c:Resources{resourceType:'food',name_DE:'Nahrung',icon:'fa-cutlery',iconColour:'#fff',colour:'#fb8c00'}) create (d:Resources{resourceType:'water',name_DE:'Wasser',icon:'fa-tint',iconColour:'#fff',colour:'#33b5e5'}) create (e:Resources{resourceType:'work',name_DE:'Arbeitskraft',icon:'fa-industry',iconColour:'#fff',colour:'#2BBBAD'}) create (f:Resources{resourceType:'overwatch',name_DE:'Überwachung',icon:'fa-eye',iconColour:'#fff',colour:'#aa66cc'}) create (g:Team{name:'red_giants'}) create (h:Team{name:'blue_dwarfs'}) create (i:Role{roleType:'ROLE_USER'}) create (j:User{uid:'pOtVsBtE', registrationDateTime:'2016-07-26 17:29:02',password:'$2y$13\$L36wWIC3GdeugizC8PaAtejVRn/FJnJlHVwSCt8mPnh0ZUtSoyRRi',isAccountNonExpired:true,isCredentialsNonExpired:true,isEnabled:true,isAccountNonLocked:true,usernameCanonical:'test',isActive:true,emailCanonical:'mfbaer@gmail.com',email:'mfbaer@gmail.com',username:'test'}) create (j)-[k:HAS_RESOURCE{amount:5}]->(a) create (j)-[l:HAS_RESOURCE{amount:15}]->(b) create (j)-[m:HAS_RESOURCE{amount:10}]->(c) create (j)-[n:HAS_RESOURCE{amount:25}]->(d) create (j)-[o:HAS_RESOURCE{amount:20}]->(e) create (j)-[p:HAS_RESOURCE{amount:30}]->(f) create (j)-[q:HAS_ROLE]->(i) create (j)-[r:IN_TEAM]->(h) create (s:User{uid:'dppnECqN',registrationDateTime:'2016-07-27 09:13:49',password:'$2y$13\$I2zRT15T0BMRA.9TK3b0oevho646UZ/ZOw3Sg/eBVS7YritYPtKWC',isAccountNonExpired:true,isCredentialsNonExpired:true,isEnabled:true,isAccountNonLocked:true,usernameCanonical:'admin',isActive:true,emailCanonical:'manuel.baer@geonet.ch',email:'manuel.baer@geonet.ch',username:'admin'}) create (t:Role{roleType:'ROLE_ADMIN'}) create (s)-[u:HAS_ROLE]->(i) create (s)-[v:HAS_ROLE]->(t)");
+        echo '<p style="color:green">TEST DB RECREATED</p><br>';  
+       
+
+        
         // echo 'Loading user with name: test <br>';        
         // $user=$em->getRepository(User::class)->findOneBy('username','test');
         // if($user){
@@ -237,15 +248,6 @@ class TestController extends Controller
         echo '<p style="color:green">OK</p><br>';        
         
         
-       // DROP ALL DATA IN GRAPH !!!CAREFULL!!!:
-       $em->getDatabaseDriver()->run("match (n)-[r]-() delete r,n");
-       $em->getDatabaseDriver()->run("match (n) delete n");
-       echo '<p style="color:green">DB CLEARED</p><br>';        
-       
-       // QUERY TO CREATE TEST DATA IN DB
-       $em->getDatabaseDriver()->run("create (a:Resources{resourceType:'wood',name_DE:'Holz',icon:'fa-tree',iconColour:'#fff',colour:'#00C851'}) create (b:Resources{resourceType:'stone',name_DE:'Stein',icon:'fa-cubes',iconColour:'#fff',colour:'#a1887f'}) create (c:Resources{resourceType:'food',name_DE:'Nahrung',icon:'fa-cutlery',iconColour:'#fff',colour:'#fb8c00'}) create (d:Resources{resourceType:'water',name_DE:'Wasser',icon:'fa-tint',iconColour:'#fff',colour:'#33b5e5'}) create (e:Resources{resourceType:'work',name_DE:'Arbeitskraft',icon:'fa-industry',iconColour:'#fff',colour:'#2BBBAD'}) create (f:Resources{resourceType:'overwatch',name_DE:'Überwachung',icon:'fa-eye',iconColour:'#fff',colour:'#aa66cc'}) create (g:Team{name:'red_giants'}) create (h:Team{name:'blue_dwarfs'}) create (i:Role{roleType:'ROLE_USER'}) create (j:User{uid:'pOtVsBtE', registrationDateTime:'2016-07-26 17:29:02',password:'$2y$13\$L36wWIC3GdeugizC8PaAtejVRn/FJnJlHVwSCt8mPnh0ZUtSoyRRi',isAccountNonExpired:true,isCredentialsNonExpired:true,isEnabled:true,isAccountNonLocked:true,usernameCanonical:'test',isActive:true,emailCanonical:'mfbaer@gmail.com',email:'mfbaer@gmail.com',username:'test'}) create (j)-[k:HAS_RESOURCE{amount:5}]->(a) create (j)-[l:HAS_RESOURCE{amount:15}]->(b) create (j)-[m:HAS_RESOURCE{amount:10}]->(c) create (j)-[n:HAS_RESOURCE{amount:25}]->(d) create (j)-[o:HAS_RESOURCE{amount:20}]->(e) create (j)-[p:HAS_RESOURCE{amount:30}]->(f) create (j)-[q:HAS_ROLE]->(i) create (j)-[r:IN_TEAM]->(h) create (s:User{uid:'dppnECqN',registrationDateTime:'2016-07-27 09:13:49',password:'$2y$13\$I2zRT15T0BMRA.9TK3b0oevho646UZ/ZOw3Sg/eBVS7YritYPtKWC',isAccountNonExpired:true,isCredentialsNonExpired:true,isEnabled:true,isAccountNonLocked:true,usernameCanonical:'admin',isActive:true,emailCanonical:'manuel.baer@geonet.ch',email:'manuel.baer@geonet.ch',username:'admin'}) create (t:Role{roleType:'ROLE_ADMIN'}) create (s)-[u:HAS_ROLE]->(i) create (s)-[v:HAS_ROLE]->(t)");
-       echo '<p style="color:green">TEST DB RECREATED</p><br>';  
-       
        die;
        return false;
     }
