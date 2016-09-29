69cfedfbe7ce2f9bf054779ee9269d86cb66c5f3
mfbaer@gmail.com <mbaer@geonet.ch>
2016-09-29 17:43:43 +0200

    bug fix

--- src/AppBundle/Controller/TeamController.php
+++ src/AppBundle/Controller/TeamController.php
@@ -31,7 +31,7 @@ class TeamController extends Controller
                 $user->addTeam($team, time());
                 $em->flush();  
                 
-                $url = $this->generateUrl('dashboard');            
+                $url = $this->generateUrl('map');
                 return new RedirectResponse($url);
             }
         }else{
